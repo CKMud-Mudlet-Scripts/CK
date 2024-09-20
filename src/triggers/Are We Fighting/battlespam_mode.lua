831fc733b1491a5b32a4d635e74934c2c86ad5b0
Jason Fried <me@jasonfried.info>
2024-09-19 23:48:10 -0700

    Workspace + fix to battlerspam style trigger

--- src/triggers/Are We Fighting/battlespam_mode.lua
+++ src/triggers/Are We Fighting/battlespam_mode.lua
@@ -1,5 +1,5 @@
 local matches = multimatches[2]
-local name = CK.Player.name or API:constant("name")
+local name = CK.Player.name or CK.API:constant("name")
 
 if matches[3] == name or matches[4] == name then
     CK.API:iThinkWeFighting()
