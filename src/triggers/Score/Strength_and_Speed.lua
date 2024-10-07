92fa8c2b6863521c257e65ae149607cb1c04f248
Jason Fried <me@jasonfried.info>
2024-10-06 20:02:40 -0700

    Swap SPD in player stats/basestats

--- src/triggers/Score/Strength_and_Speed.lua
+++ src/triggers/Score/Strength_and_Speed.lua
@@ -3,5 +3,5 @@ local Player = CK.Player
 
 Player.BaseStats.STR = tonumber(matches[2]:trim())
 Player.Stats.STR = tonumber(matches[3]:trim())
-Player.Stats.SPD = tonumber(matches[4]:trim())
-Player.BaseStats.SPD = tonumber(matches[5]:trim())
+Player.BaseStats.SPD = tonumber(matches[4]:trim())
+Player.Stats.SPD = tonumber(matches[5]:trim())
