8d90c77cbe83e1bcc1df692e0b246ab11774df7f
Jason Fried <me@jasonfried.info>
2024-10-07 16:08:36 -0700

    msdp fix active_buff_trigger.lua

--- src/scripts/Event Handlers/MSDP.lua
+++ src/scripts/Event Handlers/MSDP.lua
@@ -4,7 +4,7 @@ local names = {"LEVEL", "RACE", "POWERLEVEL", "POWERLEVEL_MAX", "KI", "KI_MAX", 
                "GODKI_MAX", "DARK_ENERGY", "AFFECTS", "ZENNI", "TOKENS", "EPOINTS", "MAX_GRAVITY", "HITROLL", "DAMROLL",
                "ARMOR", "STR", "INT", "WIS", "SPD", "CON", "STR_BASE", "INT_BASE", "WIS_BASE", "SPD_BASE", "CON_BASE",
                "OPPONENT_HEALTH", "OPPONENT_HEALTH_MAX", "OPPONENT_LEVEL", "OPPONENT_NAME", "AREA_NAME", "ROOM_EXITS",
-               "ROOM_NAME", "ROOM_VNUM", "WORLD_TIME", "SERVER_TIME", "CHARACTER_NAME"}
+               "ROOM_NAME", "ROOM_VNUM", "CHARACTER_NAME", "THIRST", "HUNGER", "ROOM_GRAVITY", "BASE_PL" }
 
 local function issue_report()
     sendMSDP("REPORT", unpack(names))
--- src/triggers/Status/Active_Buff_Trigger.lua
+++ src/triggers/Status/Active_Buff_Trigger.lua
@@ -1,7 +1,7 @@
 local matches = multimatches[2]
 local affect = string.trim(matches.affect)
 local timeleft = (tonumber(string.trim(matches.time)) or 1) * 60
-if not CK.Toggles.hide_status
+if not CK.Toggles.hide_status then
     cecho("<green> " .. timeleft .. " seconds")
 end
 CK.PromptFlags.affects[affect] = true
