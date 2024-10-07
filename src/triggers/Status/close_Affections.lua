b7d942b69d8fe8a9421cbdce3884b10038b81937
Jason Fried <me@jasonfried.info>
2024-10-07 13:35:00 -0700

    Moved from deleteline to gagline

--- src/triggers/Status/close_Affections.lua
+++ src/triggers/Status/close_Affections.lua
@@ -1,8 +1,8 @@
 setTriggerStayOpen("Status Affections", 0)
 if CK.Toggles.hide_status then
   CK.Toggles.hide_status = nil
-  deleteLine()
+  gagLine()
   -- This will delete all the lines after affections 
   -- but also the empty line and next prompt
-  tempLineTrigger(1, 9, [[ deleteLine() ]])
+  tempLineTrigger(1, 9, [[ gagLine() ]])
 end
