4500056f635df05b9f9af77f2d01acdda92062d1
Jason Fried <me@jasonfried.info>
2024-10-10 22:45:12 -0700

    Ultimate Damage in dmg formula

--- src/scripts/Damage.lua
+++ src/scripts/Damage.lua
@@ -6,8 +6,11 @@ ck:get_table("Player.Stats")
 ck:get_table("Player.BaseStats")
 
 
-local function dam(stat1, stat2, supreme, boosted)
+local function dam(stat1, stat2, supreme, boosted, ultimate)
     local supreme_multi = supreme and 7500 or 5000
+    if ultimate then
+        supreme_multi = supreme_multi * 2
+    end
     local boost_multi = (boosted or supreme) and 750 or 500
     return
         math.floor(
@@ -23,10 +26,10 @@ function API:heal_factor()
     return Player.Stats.INT * 5000 + Player.Stats.WIS * 25000 + Player.MaxPl / 100
 end
 
-function API:ki_dam(supreme, boosted)
-    return dam(Player.Stats.INT, Player.Stats.WIS, supreme, boosted)
+function API:ki_dam(supreme, boosted, ultimate)
+    return dam(Player.Stats.INT, Player.Stats.WIS, supreme, boosted, ultimate)
 end
 
-function API:phy_dam(supreme, boosted)
-    return dam(Player.Stats.STR, Player.Stats.SPD, supreme, boosted)
+function API:phy_dam(supreme, boosted, ultimate)
+    return dam(Player.Stats.STR, Player.Stats.SPD, supreme, boosted, ultimate)
 end
--- src/triggers/Score/Add_Item_Tier.lua
+++ src/triggers/Score/Add_Item_Tier.lua
@@ -7,4 +7,6 @@ cecho(string.format('<dim_gray>PHY DAM(B)<white>:      <yellow>%-18s<dim_gray>KI
     math.format(API:phy_dam(false, true)), math.format(API:ki_dam(false, true))))
 cecho(string.format('<dim_gray>PHY DAM(S)<white>:      <yellow>%-18s<dim_gray>KI DAM(S)<white>: <yellow>%s\n',
     math.format(API:phy_dam(true)), math.format(API:ki_dam(true))))
+cecho(string.format('<dim_gray>PHY DAM(U)<white>:      <yellow>%-18s<dim_gray>KI DAM(U)<white>: <yellow>%s\n',
+    math.format(API:phy_dam(true, true, true)), math.format(API:ki_dam(true, true, true))))
 cecho(string.format('<dim_gray>Item Tier<white>: <green>%7s\n', API:item_tier()))
