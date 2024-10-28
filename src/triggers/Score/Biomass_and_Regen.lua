3cadd7c85282d0c574a327ab2c80546e1d9665d3
Jason Fried <me@jasonfried.info>
2024-10-28 14:15:33 -0700

    Fix BioMAss Regen on Score Trigger

--- src/triggers/Score/Biomass_and_Regen.lua
+++ src/triggers/Score/Biomass_and_Regen.lua
@@ -1,9 +1,10 @@
 local matches = multimatches[2]
 local Player = CK.Player
 
-if Player.MaxBiomass then -- Is it known
-    local bm = math.floor(matches[2] / 100 * Player.MaxBiomass)
-    local regen = math.floor(Player.MaxBiomass * matches[3] / 100)
+if Player.MaxKi then -- Is it known
+    local bm = math.floor(matches[2] / 100 * Player.MaxKi)
+    local regen = math.floor(Player.MaxKi * matches[3] / 100)
+    Player.KiRegen = regen
     gagLine()
 
     cecho(string.format('\n<dim_gray>BioMass<white>:    %-22s<dim_gray> BM REGEN<white>: %s\n', math.format(bm),
