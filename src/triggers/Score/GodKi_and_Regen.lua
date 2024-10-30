73876ba0d95e3a054f9f471f4d69214f0d00cadb
Jason Fried <me@jasonfried.info>
2024-10-30 00:15:32 -0700

    Add GodKi Regen Calculator

---
+++ src/triggers/Score/GodKi_and_Regen.lua
@@ -0,0 +1,12 @@
+local matches = multimatches[2]
+local Player = CK.Player
+
+if Player.MaxKi then -- Is it known
+    local bm = Player.GK
+    local regen = math.floor(Player.MaxGK * matches[3] / 100)
+    Player.GKRegen = regen
+    gagLine()
+
+    cecho(string.format('\n<dim_gray>Ki<white>:    %-27s<dim_gray> KI REGEN<white>: %s\n', math.format(bm),
+        math.format(regen)))
+end
