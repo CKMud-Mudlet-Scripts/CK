d62012ff6e376ae9b07edb8a72c065915e86960f
Jason Fried <me@jasonfried.info>
2024-11-01 14:11:31 -0700

    On uninstall remove our lua packages

--- src/resources/init.lua
+++ src/resources/init.lua
@@ -272,6 +272,12 @@ registerNamedEventHandler(PREFIX, "sysUninstall", "sysUninstall", function(event
     if name:starts(PREFIX) then
         versions[name] = nil
     end
+
+    if name == "CK" then
+        -- Unload all our packages
+        package.loaded["CK"] = nil
+        package.loaded["CK.console"] = nil
+    end
 end)
 
 return ck
