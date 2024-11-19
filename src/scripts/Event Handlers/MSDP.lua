a13797d8f0bea2337da8e32ae29e2279dfa5a666
Jason Fried <me@jasonfried.info>
2024-11-19 15:29:17 -0800

    Add CMD_STACK msdp var for future use

--- src/scripts/Event Handlers/MSDP.lua
+++ src/scripts/Event Handlers/MSDP.lua
@@ -15,7 +15,7 @@ local names = {"LEVEL", "RACE", "POWERLEVEL", "POWERLEVEL_MAX", "KI", "KI_MAX", 
                "ARMOR", "STR", "INT", "WIS", "SPD", "CON", "STR_BASE", "INT_BASE", "WIS_BASE", "SPD_BASE", "CON_BASE",
                "OPPONENT_HEALTH", "OPPONENT_HEALTH_MAX", "OPPONENT_LEVEL", "OPPONENT_NAME", "AREA_NAME", "ROOM_EXITS",
                "ROOM_NAME", "ROOM_VNUM", "CHARACTER_NAME", "THIRST", "HUNGER", "ROOM_GRAVITY", "BASE_PL", "UBS", "LBS",
-               "UPDATE_EPOCH", "MAX_DENERGY"}
+               "UPDATE_EPOCH", "MAX_DENERGY", "CMD_STACK"}
 
 function MSDP:last_update()
     return getEpoch() - ((msdp or {}).UPDATE_EPOCH or 0)
